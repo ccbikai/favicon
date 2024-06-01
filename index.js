9e462f7fac1a0804e0328620f976a0c3d046b626
ccbikai <ccbikai@qq.com>
2024-06-01 22:11:52 +0800

    chore: Update .gitignore and package.json

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ let cache = caches.default
 
 const svgFavicon = 'data:image/svg+xml,'
 
-const defaultIconSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#4a5568">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
-</svg>`
+const defaultIconSvg = `<svg viewBox="0 0 20 20"><path fill="currentColor" d="M5.719 14.75a.997.997 0 0 1-.664-.252L-.005 10l5.341-4.748a1 1 0 0 1 1.328 1.495L3.005 10l3.378 3.002a1 1 0 0 1-.664 1.748zm8.945-.002L20.005 10l-5.06-4.498a.999.999 0 1 0-1.328 1.495L16.995 10l-3.659 3.252a1 1 0 0 0 1.328 1.496zm-4.678 1.417 2-12a1 1 0 1 0-1.972-.329l-2 12a1 1 0 1 0 1.972.329z"></path></svg>`
 
 async function handleRequest(request) {
   const init = {
@@ -17,7 +15,13 @@ async function handleRequest(request) {
 
   const url = requestURL.searchParams.get('url')
 
-  const targetURL = new URL(url.startsWith('https') ? url : 'https://' + url)
+  if (!url) {
+    return new Response('Please provide a URL', {
+      status: 400,
+    })
+  }
+
+  const targetURL = new URL(url.startsWith('http') ? url : 'https://' + url)
 
   let favicon = ''
   const response = await fetch(targetURL.origin, init).catch(() => {
